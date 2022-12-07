3c4b8f48054dddc1760cede7283a4b40d3cd8c8c
hasuhwan <hapung215@gmail.com>
2022-12-07 22:11:53 +0900

    b

--- pages/api/signInRequest.js
+++ pages/api/signInRequest.js
@@ -15,20 +15,21 @@ export default async function signInRequest(req, res) {
       todo: [],
     });
     console.log(insertUser); //insertone 정보 넣기 // findone 찾기
-    // if (userData.every((el) => el.userid !== userid)) {
-    //   userData.push({
-    //     username,
-    //     userid,
-    //     password,
-    //     todo: [],
-    //   });
-    //   res.status(201).send(userData[userData.length - 1]);
-    // } else {
-    //   res.status(401).send("same id");
-    // }
   } catch (e) {
     console.error(e);
   } finally {
     await client.close();
   }
 }
+
+// if (userData.every((el) => el.userid !== userid)) {
+//   userData.push({
+//     username,
+//     userid,
+//     password,
+//     todo: [],
+//   });
+//   res.status(201).send(userData[userData.length - 1]);
+// } else {
+//   res.status(401).send("same id");
+// }
